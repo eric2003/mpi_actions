78da7af6040b426c3294b9038d39b2db1076da27
eric <fantasy_2003_@hotmail.com>
2021-07-11 14:03:41 +0800

    mpi actions modify 6

--- codes/app/src/app.cpp
+++ codes/app/src/app.cpp
@@ -12,7 +12,7 @@ int main( int argc, char ** argv )
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Get_processor_name( processor_name, & namelen );
     //printf("Hello World! Process %d of %d on %s\n", myid,numprocs,processor_name);
-	cout << "Hello World! Process " << myid << " of " << numprocs << on << processor_name << "\n";
+	cout << "Hello World! Process " << myid << " of " << numprocs << " on " << processor_name << "\n";
     MPI_Finalize();
 	
     return 0;
