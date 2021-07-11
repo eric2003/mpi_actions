6af861dffed1008b7c582eb38877b33fbc500909
eric <fantasy_2003_@hotmail.com>
2021-07-11 15:26:21 +0800

    mpi actions modify 10

--- .github/workflows/install_msmpi.ps1
+++ .github/workflows/install_msmpi.ps1
@@ -1,22 +1,22 @@
 function test() {
     Write-Host "Hello, World!"
-	Write-Host "Hello, Github actions!"
-	Write-Host "Hello, OneFLOW-CFD!"
+    Write-Host "Hello, Github actions!"
+    Write-Host "Hello, OneFLOW-CFD!"
 }
 
 function MyGetFileName( $filePath ) {
-	$file_name_complete = [System.IO.Path]::GetFileName("$filePath")
-	$file_name_complete
-	#Write-Host "fileNameFull :" $file_name_complete	
+    $file_name_complete = [System.IO.Path]::GetFileName("$filePath")
+    $file_name_complete
+    #Write-Host "fileNameFull :" $file_name_complete    
 }
 
 function MyDownloadFile( $fullFilePath ) {
-	$my_filename = MyGetFileName("$fullFilePath")
-	$my_location = Get-Location
-	$my_local_filename = "$my_location" + "/" + $my_filename
-	
-	$my_client = new-object System.Net.WebClient
-	$my_client.DownloadFile( $fullFilePath, $my_local_filename )	
+    $my_filename = MyGetFileName("$fullFilePath")
+    $my_location = Get-Location
+    $my_local_filename = "$my_location" + "/" + $my_filename
+    
+    $my_client = new-object System.Net.WebClient
+    $my_client.DownloadFile( $fullFilePath, $my_local_filename )    
 }
 
 function InstallMSMPI() {
@@ -24,40 +24,41 @@ function InstallMSMPI() {
     cd mpi_dir
     # install MPI SDK and Runtime
     Write-Host "Installing Microsoft MPI SDK..."
-	$download_url = "https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/"
-	$msmpisdk_filename = "msmpisdk.msi"
-	$msmpisdk_webfilename = $download_url + $msmpisdk_filename
+    $download_url = "https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/"
+    $msmpisdk_filename = "msmpisdk.msi"
+    $msmpisdk_webfilename = $download_url + $msmpisdk_filename
 
-	MyDownloadFile( $msmpisdk_webfilename )
-	
+    MyDownloadFile( $msmpisdk_webfilename )
+    
     Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i msmpisdk.msi" -Wait
     Write-Host "Microsoft MPI SDK installation complete"
-	
+    
     Write-Host "Installing Microsoft MPI Runtime..."
-	$msmpisetup_filename = "msmpisetup.exe"
-	$msmpisetup_webfilename = $download_url + $msmpisetup_filename
-	
-	MyDownloadFile( $msmpisetup_webfilename )
-	
+    $msmpisetup_filename = "msmpisetup.exe"
+    $msmpisetup_webfilename = $download_url + $msmpisetup_filename
+    
+    MyDownloadFile( $msmpisetup_webfilename )
+    
     Start-Process -FilePath MSMpiSetup.exe -ArgumentList -unattend -Wait
     Write-Host "Microsoft MPI Runtime installation complete..."
-	
-	$msmpi_bin_path = "C:/Program Files/Microsoft MPI/Bin"
-	$msmpi_sdk_path = "C:/Program Files (x86)/Microsoft SDKs/MPI"
-	
-	$Env:path = $Env:Path + ";$msmpi_bin_path"
-	Write-Host "ls $msmpi_sdk_path"
-	ls $msmpi_sdk_path
-	Write-Host "ls $msmpi_sdk_path"
-	ls $msmpi_bin_path
-	
-	Write-Host "mpiexec"
-	mpiexec
+    
+    $msmpi_bin_path = "C:/Program Files/Microsoft MPI/Bin"
+    $msmpi_sdk_path = "C:/Program Files (x86)/Microsoft SDKs/MPI"
+    
+    $Env:path = $Env:Path + ";$msmpi_bin_path
+	[environment]::SetEnvironmentvariable("Path", $Env:path, "User")
+    Write-Host "ls $msmpi_sdk_path"
+    ls $msmpi_sdk_path
+    Write-Host "ls $msmpi_sdk_path"
+    ls $msmpi_bin_path
+    
+    Write-Host "mpiexec"
+    mpiexec
 }
 
 function main() {
     #test
-	InstallMSMPI
+    InstallMSMPI
 }
 
 main
